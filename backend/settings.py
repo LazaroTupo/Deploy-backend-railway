920e9f8011c905422f7d902f86fb7079af8a8454
Daniel Lorenzo <danielalorenzoramos@gmail.com>
2023-11-16 22:57:16 -0500

    url railway frontend

--- backend/settings.py
+++ backend/settings.py
@@ -146,7 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'https://railway-frontend-production-cf8e.up.railway.app',
-    'http://stunning-baklava-dc64df.netlify.app/'
+    'railway-frontend-production-e20f.up.railway.app'
 ]
 
 REST_FRAMEWORK = {
